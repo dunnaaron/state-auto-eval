db19bfa2687f9939105a105aa358748ef6e21c98
Aaron Dunn <dunnac1@gmail.com>
2020-07-16 19:16:36 -0400

    work done during eval

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-const helloWorld = () => 'Hello World!'
+const createPassengers = () => {
+    const passengers = new Array(100).fill({})
 
-module.exports = { helloWorld }
+    return passengers.map((passenger, index) => {
+        return {
+            id: index,
+            assignedSeat: index,
+            occupiedSeat: null
+        }
+    })
+}
+
+const airlineSeats = () => {
+    const passengers = createPassengers()
+    const seats = new Array(100).fill(false)
+
+    passenger.forEach((passenger, index) => {
+        if (passenger.id === 0) {
+            const randomSeat = Math.floor(Math.random() * Math.floor(100))
+
+            passenger.occupiedSeat = randomSeat
+            seats
+        }
+    })
+
+    
+    return
+}
+
+module.exports = { airlineSeats, createPassengers }
--- index.test.js
+++ index.test.js
@@ -1,8 +1,25 @@
 const { expect } = require('chai')
-const { helloWorld } = require('./index')
+const { airlineSeats, createPassengers } = require('./index')
 
-describe('helloWorld', () => {
-    it('returns Hello World!', () => {
-        expect(helloWorld()).to.equal('Hello World!')
+describe('airline seats', () => {
+    it('returns true if last passenger is in assigned seat', () => {
+        const lastPassenger = airlineSeats()
+        const result = lastPassenger.assignedSeat === lastPassenger.occupiedSeat
+
+        expect(result).to.equal(true)
+    })
+})
+
+describe('createPassengers', () => {
+    it('builds passenger array with 100 passenger objects', () => {
+        const passengers = createPassengers()
+        console.log('PASSENGERS: ', passengers)
+
+        expect(passengers.length).to.equal(100)
+        expect(passengers[0]).to.deep.equal({
+            id: 0,
+            assignedSeat: 0,
+            occupiedSeat: null
+        })
     })
 })
